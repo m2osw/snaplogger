d4a4238405de896a2ebe61e35632eba4c75e9423
AlexisWilke <alexis@m2osw.com>
2022-05-17 18:13:45 -0700

    Cleaned up cmake file.

--- cmake/SnapLoggerConfig.cmake
+++ cmake/SnapLoggerConfig.cmake
@@ -1,11 +1,20 @@
-# Copyright (c) 2013-2022  Made to Order Software Corp.  All Rights Reserved
+# - Try to find SnapLogger
+#
+# SNAPLOGGER_FOUND        - System has SnapLogger
+# SNAPLOGGER_INCLUDE_DIRS - The SnapLogger include directories
+# SNAPLOGGER_LIBRARIES    - The libraries needed to use SnapLogger
+# SNAPLOGGER_DEFINITIONS  - Compiler switches required for using SnapLogger
+#
+# License:
+#
+# Copyright (c) 2011-2022  Made to Order Software Corp.  All Rights Reserved
 #
 # https://snapwebsites.org/project/snaplogger
 # contact@m2osw.com
-# 
-# This program is free software; you can redistribute it and/or modify
+#
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -14,25 +23,15 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-#
-# - Try to find SnapLogger
-#
-# Once done this will define
-#
-# SNAPLOGGER_FOUND        - System has SnapLogger
-# SNAPLOGGER_INCLUDE_DIRS - The SnapLogger include directories
-# SNAPLOGGER_LIBRARIES    - The libraries needed to use SnapLogger (none)
-# SNAPLOGGER_DEFINITIONS  - Compiler switches required for using SnapLogger (none)
-#
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 
 find_path(
     SNAPLOGGER_INCLUDE_DIR
         snaplogger/logger.h
 
     PATHS
-        $ENV{SNAPLOGGER_INCLUDE_DIR}
+        ENV SNAPLOGGER_INCLUDE_DIR
 )
 
 find_library(
@@ -40,7 +39,8 @@ find_library(
         snaplogger
 
     PATHS
-        $ENV{SNAPLOGGER_LIBRARY}
+        ${SNAPLOGGER_LIBRARY_DIR}
+        ENV SNAPLOGGER_LIBRARY
 )
 
 mark_as_advanced(
@@ -49,17 +49,15 @@ mark_as_advanced(
 )
 
 set(SNAPLOGGER_INCLUDE_DIRS ${SNAPLOGGER_INCLUDE_DIR})
-set(SNAPLOGGER_LIBRARIES    ${SNAPLOGGER_LIBRARY}    )
+set(SNAPLOGGER_LIBRARIES    ${SNAPLOGGER_LIBRARY})
+
 
 include(FindPackageHandleStandardArgs)
-
-# handle the QUIETLY and REQUIRED arguments and set SNAPLOGGER_FOUND to TRUE
-# if all listed variables are TRUE
 find_package_handle_standard_args(
     SnapLogger
-    DEFAULT_MSG
-    SNAPLOGGER_INCLUDE_DIR
-    SNAPLOGGER_LIBRARY
+    REQUIRED_VARS
+        SNAPLOGGER_INCLUDE_DIR
+        SNAPLOGGER_LIBRARY
 )
 
 # vim: ts=4 sw=4 et
